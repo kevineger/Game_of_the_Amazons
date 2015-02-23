7394d8661e5da66238ab3681f10954f4edf03419
kevineger <egerkevinjames@gmail.com>
2015-02-23 12:42:03 -0800

    Adds core GUI

---
+++ GameBoard.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kevin
+ *
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameBoard extends JFrame {
+	JSplitPane Pane;
+    JPanel Board;
+
+    JTextArea console = new JTextArea();
+    private int xShift;
+    private int yShift;
+    final Dimension paneSize = new Dimension(1000, 1000);
+    final Dimension boardSize = new Dimension(800,800);
+    final Dimension textAreaSize = new Dimension(400,400);
+    
+//    GameBoard constructor
+    public GameBoard() {
+    	Board = new JPanel();
+    	Board.setLayout(new GridLayout(8, 8));
+        Board.setPreferredSize(boardSize);
+        Board.setBounds(0, 0, boardSize.width, boardSize.height);
+        
+//        Draw Board sqaures
+        for (int i = 0; i < 64; i++) {
+            JPanel square = new JPanel(new BorderLayout());
+            Board.add(square);
+            int row = (i / 8) % 2;
+            if (row == 0) {
+            	square.setBackground( i % 2 == 0 ? Color.white : Color.gray);
+            }               
+            else {
+            	square.setBackground( i % 2 == 0 ? Color.gray: Color.white);
+            }                
+        }
+
+        // Add Text Area
+        console.setBounds(0,0, textAreaSize.width, textAreaSize.height);
+
+        //Create split pane
+        Pane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, Board, console);
+        Pane.setPreferredSize(paneSize);
+
+        //Pieces
+        JLabel Piece_WHITE = new JLabel(new ImageIcon("./images/nic.png"));
+        JLabel Piece_BLACK = new JLabel(new ImageIcon("./images/nic2.png"));
+
+        /*
+        Test Display Pieces
+         */
+
+        JPanel panel2 = (JPanel)Board.getComponent(0);
+        panel2.add(Piece_WHITE);
+
+        JPanel panel = (JPanel)Board.getComponent(3);
+        panel.add(Piece_BLACK);
+
+        //add to display
+        getContentPane().add(Pane);
+    	
+    }
+
+
+    public void write(String message){
+        console.append(message);
+    }
+
+    public String read(){
+        return console.getText();
+    }
+}
--- GamePlayer.java
+++ GamePlayer.java
@@ -19,7 +19,7 @@ import ubco.ai.games.GameMessage;
 public class GamePlayer implements ubco.ai.games.GamePlayer {
 
 	GameClient gameClient = null; 
-	
+	GameBoard frame = null;
 	
 	/*
 	 * Constructor 
@@ -29,6 +29,12 @@ public class GamePlayer implements ubco.ai.games.GamePlayer {
 		//A player has to maintain an instance of GameClient, and register itself with the  
 		//GameClient. Whenever there is a message from the server, the Gameclient will invoke 
 		//the player's handleMessage() method.
+		
+		frame = new GameBoard();
+    	frame.pack();
+    	frame.setResizable(true);
+    	frame.setLocationRelativeTo( null );
+    	frame.setVisible(true);
 		
 		//Three arguments: user name (any), passwd (any), this (delegate)   
 	    gameClient = new GameClient(name, passwd, this);
@@ -47,19 +53,52 @@ public class GamePlayer implements ubco.ai.games.GamePlayer {
 		Scanner in = new Scanner(System.in);
 		System.out.println("\nWhich room would you like to join?");
 		gameClient.joinGameRoom(in.nextLine());
+		
+		String msg = "";
+		msg = "<action type='"+GameMessage.MSG_CHAT+"'>"+ "Testing a chat message yo" +"</action>";
+		String messageToSend = ServerMessage.compileGameMessage(GameMessage.MSG_CHAT, 8, msg);
+		gameClient.sendToServer(messageToSend);
 	}
 	
 	//general message from the server
-	public boolean handleMessage(String arg0) throws Exception {
-		System.out.println("Game message: "+arg0);
+	public boolean handleMessage(String msg) throws Exception {
+		System.out.println("Game message: "+msg);
 		return true;
 	}
 
 	//game-specific message from the server
-	public boolean handleMessage(GameMessage arg0) throws Exception {
-		IXMLElement xml = ServerMessage.parseMessage(arg0.msg); 
+	public boolean handleMessage(GameMessage gmsg) throws Exception {
+		IXMLElement xml = ServerMessage.parseMessage(gmsg.msg); 
 		String xmlType = xml.getAttribute("type", null);
-		System.out.println("THE TYPE IS: " + xmlType);
+		System.out.println("Message Type: " + xmlType);
+		
+		switch(xmlType) {
+		case GameMessage.ACTION_GAME_START:
+			System.out.println("Message Type: ACTION_GAME_START");
+			
+			break;
+		case GameMessage.ACTION_MOVE:
+			System.out.println("Message Type: ACTION_MOVE");
+			break;
+		case GameMessage.ACTION_POS_MARKED:
+			System.out.println("Message Type: ACTION_POS_MARKED");
+			break;
+		case GameMessage.ACTION_ROOM_JOINED:
+			System.out.println("Message Type: ACTION_ROOM_JOINED");
+			break;
+		case GameMessage.MSG_CHAT:
+			System.out.println("Message Type: MSG_CHAT");
+			break;
+		case GameMessage.MSG_GAME:
+			System.out.println("Message Type: MSG_GAME");
+			break;
+		case GameMessage.MSG_GENERAL:
+			System.out.println("Message Type: MSG_GENERAL");
+			break;
+		case GameMessage.MSG_JOIN_ROOM:
+			System.out.println("Message Type: MSG_JOIN_ROOM");
+			break;
+		}
 		return true;
 	}
 
@@ -72,5 +111,6 @@ public class GamePlayer implements ubco.ai.games.GamePlayer {
 	  // and (3) call the method gameClient.sendToServer() to send the compiled message.
 		
 	  // For message types and message format, see the GameMessage API and the project notes	
+
 	}
 }
